a2843f64aa951c72f67846ae5690725d93e06c2b
Rodion <rodionserez@gmail.com>
2023-03-19 16:24:00 +0300

    feat: getting the weather and passing it to another component

--- src/components/weatherComponent.vue
+++ src/components/weatherComponent.vue
@@ -1,38 +1,45 @@
 <template>
-	<button @click="getWeather"> get Weather</button>
+	
 </template>
 
 <script>
 import axios from 'axios';
+// import { emit } from 'process-nextick-args';
 
 export default {
 	data() {
 		return {
 			APIKEY: '9b7de6c7e4b137f586b5a0aaf630a7f0',
-			city: 'Minsk',
 		}
 	},
 	//`api.openweathermap.org/data/2.5/forecast/daily?city=${this.city}&cnt=16&appid=${this.APIKEY}`
 	methods: {
-		async getWeather() {
-			const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&lang=en&appid=${this.APIKEY}&units=metric`; //&mode=json
-			const url5days = `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${this.APIKEY}`
+		async getWeather(location) {
+			const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=en&appid=${this.APIKEY}&units=metric`; //&mode=json
+			const url5days = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${this.APIKEY}`
 			
 			try {
 				const response = await axios.get(url5days);
 				console.log(response);
 				console.log(response.data);
 				console.log(response.data.list[0]);
-				// const response2 = await fetch(url);
-				// console.log(response2.json())
+				return response
 			} catch(err) {
-				alert(err);
+				console.log(err);
 			}
 		}
+	},
+
+	props: {
+		location: String
+	},
+
+	watch: {
+		async location(location) {
+			const weatherData = await this.getWeather(location);
+			this.$emit('getWeather', weatherData);
+		}
 	}
+
 }
 </script>
-
-<style>
-
-</style>
