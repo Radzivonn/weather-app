6b1fad3096e01b605201b61cb60b1e9f1a619ba2
Rodion <rodionserez@gmail.com>
2023-06-13 22:44:40 +0300

    feat: add weather getter function

--- src/components/weatherComponent.vue
+++ src/components/weatherComponent.vue
@@ -15,13 +15,27 @@ export default {
 		async getWeather(url) {
 			try {
 				const response = await axios.get(url);
-				return response
-			} catch(err) {
+				return this.getSearchData(response);
+			} catch (err) {
 				console.log(err);
 			}
-		}
+		},
+		getSearchData(weatherData) {
+			if (weatherData === 'noLocation') return false; // If the location is entered incorrectly
+			else {
+				return {
+					isMylocation: false,
+					location: this.location,
+					forecastTime:
+						`${new Date().getHours()}:${new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes()}`,
+					WeatherIconSrc: `${weatherData.data.weather[0].main}`,
+					currentTempreture: String(Math.round(weatherData.data.main.temp)),
+					highAndLowTempreture: [Math.round(weatherData.data.main.temp_max), Math.round(weatherData.data.main.temp_min)],
+					widgetSize: 'big'
+				};
+			}
+		},
 	},
-
 	props: {
 		location: String
 	},
