5281883068d12c7d0a026b17238fc5cc36e4f82c
Rodion <rodionserez@gmail.com>
2023-09-27 23:31:14 +0300

    refactor: rename module

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "vuex";
-import { currentSearch } from "@/store/currentSearchModule";
+import { currentSearch } from "@/store/currentSearch";
 import { latestSearches } from "./latestSearches";
 
 export default createStore({
